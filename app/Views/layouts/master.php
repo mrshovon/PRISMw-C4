92c7a4cf64975b25dea353857bdc5d99f591a76d
Mushfiq <1730020@iub.edu.bd>
2023-04-04 15:50:16 +0600

    daylast

--- app/Views/layouts/master.php
+++ app/Views/layouts/master.php
@@ -1,116 +1,215 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> -->
-    <title>HOME</title>
-    <link rel="stylesheet" href="<?=base_url('public/css/style.css')?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-</head>
-<body>
-   <!-- nav-bar -->
-   <header>
-        <nav>
-            <div class="logo">
-              <img src="<?=base_url('public/images/LOGO.png')?>" alt="Logo">
-            </div>
-            <ul class="nav-links">
-              <li class="btn btn-light"><a href="<?=base_url('public/home')?>">Home</a></li>
-              <li class="btn btn-light"><a href="<?=base_url('public/home/aboutus')?>">About Us</a></li>
-              <li class="btn btn-light"><a href="<?=base_url('public/home/contactus')?>">Contact Us</a></li>
-              <li class="dropdown btn btn-light">
-                <a href="#">Services</a>
-                <div class="dropdown-content">
-                  <a href="<?=base_url('public/home/homeloan')?>">Home-Loan</a>
-                  <a href="<?=base_url('public/home/renovation')?>">Renovation</a>
-                  <a href="<?=base_url('public/home/legalservices')?>">Legal-Services</a>
-                </div>
-              </li>
-              <li class="btn btn-info"><a href="<?=base_url('public/home/signup')?>">Sign Up</a></li>
-              <li><a href="<?=base_url('public/home/login')?>" class="btn btn-primary">Login</a></li>
-            </ul>
-            <!-- menu bar -->
-          <div class="menu-bar">
-            <img onclick="myFunction2()" src="<?=base_url('public/images/menus.png')?>" alt="">
-            
-            <div id="hidden" class="menu-items" style="display: none;">
-              <ul>
-                <li ><a href="<?=base_url('public/home')?>">Home</a></li>
-                <li ><a href="<?=base_url('public/home/aboutus')?>">About Us</a></li>
-                <li ><a href="<?=base_url('public/home/contactus')?>">Contact Us</a></li>
-                <li ><a onclick="myFunction3()" href="#">Services</a></li>
-                <div id="services" style="display: none">
-                  <li> <a href="<?=base_url('public/home/homeloan')?>">Home-Loan</a></li>
-                  <li><a href="<?=base_url('public/home/renovation')?>">Renovation</a></li>
-                  <li><a href="<?=base_url('public/home/loginlegalservices')?>">Legal-Services</a></li>
-                </div>
-                <li ><a href="<?=base_url('public/home/signup')?>">Sign Up</a></li>
-                <li><a href="<?=base_url('public/home/login')?>" >Login</a></li>
-              </ul>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
+  <!-- Favicons -->
+  <link href="<?=base_url('public/img/favicon.png')?>" rel="icon">
+  <link href="<?=base_url('public/img/apple-touch-icon.png')?>" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="<?=base_url('public/lib/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="<?=base_url('public/lib/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet">
+  <link href="<?=base_url('public/lib/animate/animate.min.css')?>" rel="stylesheet">
+  <link href="<?=base_url('public/lib/ionicons/css/ionicons.min.css')?>" rel="stylesheet">
+  <link href="<?=base_url('public/lib/owlcarousel/assets/owl.carousel.min.css')?>" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="<?=base_url('public/css/style.css')?>" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand" href="index.html">PRISM<span class="color-b"> PROPERTIES</span></a>
+
+      <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact Us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Services
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="home-loan.html">Home-Loan</a>
+              <a class="dropdown-item" href="renovation.html">Renovation</a>
+              <a class="dropdown-item" href="Legal-services.html">Documentation Service</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Sign Up.html">Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Log in.html">Log in</a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+    <!-- main -->
+    
+  <?= $this->renderSection('content') ?>
+    
+  <!--/ footer Star /-->
+  <section class="section-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">PRISM PROPERTIES</h3>
+            </div>
+            <div class="w-body-a">
+              <p class="w-text-a color-text-a">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et voluptatum minima officia totam earum aperiam unde esse iste!.
+              </p>
+            </div>
+            <div class="w-footer-a">
+              <ul class="list-unstyled">
+                <li class="color-a">
+                  <span class="color-text-a">Phone .</span> contact@example.com</li>
+                <li class="color-a">
+                  <span class="color-text-a">Email .</span> +54 356 945234</li>
+              </ul>
             </div>
           </div>
-          <!-- menu bar ends -->
+        </div>
+        <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">The Company</h3>
+            </div>
+            <div class="w-body-a">
+              <div class="w-body-a">
+                <ul class="list-unstyled">
+                  <li class="item-list-a">
+                    <i class="fa fa-angle-right"></i> <a href="careers.html">Careers</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="fa fa-angle-right"></i> <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="nav-footer">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#">Home</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">About</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">contact us</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">Sign-up</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">log-in</a>
+              </li>
+            </ul>
           </nav>
-    </header>
-    <!-- nav-bar -->
-    <!-- main -->
-    <main style="height:100vh">
-        <?= $this->renderSection('content') ?>
-    </main>
-      
-    <!-- main -->
-    <!-- footer -->
-    <footer>
-        <div class="footer-container">
-          <div class="footer-section">
-            <h3>Company Information</h3>
-            <p>1234 Main Street</p>
-            <p>Anytown, USA 12345</p>
-            <p>Phone: 555-555-5555</p>
+          <div class="socials-a">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="fa fa-dribbble" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
           </div>
-          <div class="footer-section">
-            <h3>Follow Us</h3>
-            <div class="social-icons">
-              <a href="#"><i class="fa fa-facebook"></i></a>
-              <a href="#"><i class="fa fa-twitter"></i></a>
-              <a href="#"><i class="fa fa-instagram"></i></a>
-            </div>
+          <div class="copyright-footer">
+            <p class="copyright color-text-a">
+              &copy; Copyright
+              <span class="color-a">PRISM</span> All Rights Reserved.
+            </p>
           </div>
-          <div class="footer-section">
-            <h3>Careers</h3>
-            <p>Visit our Careers page for current job openings.</p>
-            <a href="<?=base_url('public/home/career')?>">Careers</a>
-          </div>
-          <div class="footer-section">
-            <h3>Privacy Policy</h3>
-            <a href="<?=base_url('public/home/privacypolicy')?>">Privacy Policy</a>
-          </div>
+          
         </div>
-        <div class="footer-bottom">
-          <p>&copy; 2023 My Company. All rights reserved.</p>
-        </div>
-      </footer>
-      
-    <!-- footer -->
-    <!-- script -->
-    <script src="<?=base_url('public/js/script.js')?>"></script>
-    <script src="https://kit.fontawesome.com/298c7a36dd.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script>
-      var input = document.getElementById("input");
-      input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-          event.preventDefault();
-          document.getElementById("clkbtn").click();
-        }
-      });
-      </script>
-  </body>
+      </div>
+    </div>
+  </footer>
+  <!--/ Footer End /-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- JavaScript Libraries -->
+  <script src="<?=base_url('public/lib/jquery/jquery.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/jquery/jquery-migrate.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/popper/popper.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/bootstrap/js/bootstrap.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/easing/easing.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/owlcarousel/owl.carousel.min.js')?>"></script>
+  <script src="<?=base_url('public/lib/scrollreveal/scrollreveal.min.js')?>"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="<?=base_url('public/contactform/contactform.js')?>"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="<?=base_url('public/js/main.js')?>"></script>
+
+</body>
 </html>
+    <!-- main -->
+    
--- app/Views/prism/home.php
+++ app/Views/prism/home.php
@@ -1,15 +1,163 @@
 <?= $this->extend('layouts/master') ?>
 <?= $this->section('content') ?>
-    <h1 class="head-tag">Welcome to PRISM Properties</h1>
-      <div class="carousel-container">
-        <div class="carousel-slide">
-          <img src="<?=base_url('public/images/demo 1.jpg')?>" alt="image1">
-          <img src="<?=base_url('public/images/demo 2.jpg')?>" alt="image2">
-          <img src="<?=base_url('public/images/demo3.jpg')?>" alt="image3">
+<div class="intro intro-carousel">
+    <div id="carousel" class="owl-carousel owl-theme">
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-1.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Banshree, Dhaka
+                      <br> 1219</p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">House 13 </span> Banashree
+                      <br> Block A, Road 3</h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">sell | tk 10,000,000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="carousel-prev"><</div>
-        <div class="carousel-next">></div>
       </div>
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-2.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Mirpur, Dhaka
+                      <br> 1216</p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">1296 </span> Mirpur
+                      <br> East Monipur</h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">rent | tk 20,000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-3.jpg)">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="intro-body">
+                    <p class="intro-title-top">Bashundhara, Dhaka
+                      <br> 1213</p>
+                    <h1 class="intro-title mb-4">
+                      <span class="color-b">38 </span> Bashundhara Residential
+                      <br> Block k, Road 2</h1>
+                    <p class="intro-subtitle intro-price">
+                      <a href="#"><span class="price-a">rent | tk 50,000</span></a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Carousel end /-->
+
+  <!--/ Services Star /-->
+  <section class="section-services section-t8">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="title-wrap d-flex justify-content-between">
+            <div class="title-box">
+              <h2 class="title-a">Our Services</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card-box-c foo">
+            <div class="card-header-c d-flex">
+              <div class="card-box-ico">
+                <span class="fa fa-home"></span>
+              </div>
+              <div class="card-title-c align-self-center">
+                <h2 class="title-c">Buy/Sell<br>and Rent  </h2>
+              </div>
+            </div>
+            <div class="card-body-c">
+              <p class="content-c">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et voluptatum minima officia totam earum aperiam unde esse iste, tempora natus soluta nulla tempore dolores! Quis tempore animi vel itaque expedita!
+              </p>
+            </div>
+            <div class="card-footer-c">
+              <a href="#" class="link-c link-icon">Read more
+                <span class="ion-ios-arrow-forward"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card-box-c foo">
+            <div class="card-header-c d-flex">
+              <div class="card-box-ico">
+                <span class="fa fa-usd"></span>
+              </div>
+              <div class="card-title-c align-self-center">
+                <h2 class="title-c">Loans</h2>
+              </div>
+            </div>
+            <div class="card-body-c">
+              <p class="content-c">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et voluptatum minima officia totam earum aperiam unde esse iste, tempora natus soluta nulla tempore dolores! Quis tempore animi vel itaque expedita!
+              </p>
+            </div>
+            <div class="card-footer-c">
+              <a href="#" class="link-c link-icon">Read more
+                <span class="ion-ios-arrow-forward"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card-box-c foo">
+            <div class="card-header-c d-flex">
+              <div class="card-box-ico">
+                <span class="fa fa-file"></span>
+              </div>
+              <div class="card-title-c align-self-center">
+                <h2 class="title-c">Documentation and Renovation</h2>
+              </div>
+            </div>
+            <div class="card-body-c">
+              <p class="content-c">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et voluptatum minima officia totam earum aperiam unde esse iste, tempora natus soluta nulla tempore dolores! Quis tempore animi vel itaque expedita!
+              </p>
+            </div>
+            <div class="card-footer-c">
+              <a href="#" class="link-c link-icon">Read more
+                <span class="ion-ios-arrow-forward"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Services End /-->
 <?= $this->endSection() ?>
     
    
